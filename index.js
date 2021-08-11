ad02cc96ac838275e21c7f5939511133a49323e4
student <studemt@students-MBP.attlocal.net>
2021-08-11 14:16:54 -0700

    created /students endpoint and added mock student data

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+
+const students = [
+  {
+    name:'Joe',
+    grades: ['A','B','C'],
+    id: 1
+   },
+   {
+    name:'John',
+    grades: ['A','A','A'],
+    id: 2
+   },
+   {
+    name:'Kenny',
+    grades: ['A','F','D'],
+    id: 3
+   },
+  {
+    name: 'Mark',
+    id: 4,
+    grades: ['B']
+  },
+  {
+    name: 'Billy', 
+    id: 5,
+    grades: ['C', 'C', 'C']
+  }
+];
+
+app.get('/students',(req,res) =>{
+  res.send(JSON.stringify(students))
+})
+
+app.listen(port,() => {
+  console.log(`App is running on port ${port}`)
+});
